e3523a42ae662661db53cd50cb45d61b09f13029
shaneehlert <shane@esperas.org>
2016-09-12 12:50:09 -0700

    view of parse json locations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,18 @@
 class HomeController < ApplicationController
 
   def index
-
+    parameters = { term: 'Bars'}
+    @results = Yelp.client.search('Seattle', parameters)
+    # render json: results
   end
 
   def new
   end
 
   def search
-    parameters = { term: 'bar'}
-
-    results = Yelp.client.search('Seattle', parameters)
-    render json: results
+    parameters = { term: 'Bars'}
+    @results = Yelp.client.search('Seattle', parameters)
+    # render json: results
   end
 
 end
