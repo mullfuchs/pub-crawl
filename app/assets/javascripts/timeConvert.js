317ba4c3a72ac0f739e413363897a2684cbf43c3
kendradalley <kendracdalley@gmail.com>
2016-09-13 16:16:01 -0700

    merge conflicts

--- app/assets/javascripts/timeConvert.js
+++ app/assets/javascripts/timeConvert.js
@@ -1,3 +1,34 @@
+// $(document).on('turbolinks:load', function() {
+// $('h4').each(function(){
+//     $(this).find('.eventDate').each(function() {
+//       console.log('.eventDate');
+//       var formattedDate = moment(dateHTML).format('dddd, MMM Do'); 
+//       $('#eventDate').html(formattedDate);
+
+//     }); // end of find function
+// }); //end of h4 loop
+
+
+// }); //end of document load function
+
+// loop over array and change h4 looking for id of event Date
+
+
+// $(document).on('turbolinks:load', function() {
+//   $('.date-format').each(function() {
+//     // console.log('h4');
+//   var dateHTML = $('#eventDate').html()
+//   console.log(dateHTML);
+//   var formattedDate = moment(dateHTML).format('dddd, MMM Do');
+//   $('#eventDate').html(formattedDate);
+//   });
+
+//   // var timeHTML = $('#eventTime').html()
+//   // var formattedTime = moment(timeHTML, "hh").format('h:00a');
+//   // $('#eventTime').html(formattedTime)
+
+// });
+
 $(document).on('turbolinks:load', function() {
   var dateHTML = $('#eventDate').html()
   var formattedDate = moment(dateHTML).format('dddd, MMM Do');
