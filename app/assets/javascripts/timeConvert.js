6dfc63889360571f5bf58c229edd3ae0f48bc70e
kendradalley <kendracdalley@gmail.com>
2016-09-13 17:25:27 -0700

    semi-fixed bug for date/time stamps

--- app/assets/javascripts/timeConvert.js
+++ app/assets/javascripts/timeConvert.js
@@ -1,41 +1,24 @@
-// $(document).on('turbolinks:load', function() {
-// $('h4').each(function(){
-//     $(this).find('.eventDate').each(function() {
-//       console.log('.eventDate');
-//       var formattedDate = moment(dateHTML).format('dddd, MMM Do'); 
-//       $('#eventDate').html(formattedDate);
-
-//     }); // end of find function
-// }); //end of h4 loop
-
-
-// }); //end of document load function
-
-// loop over array and change h4 looking for id of event Date
-
-
-// $(document).on('turbolinks:load', function() {
-//   $('.date-format').each(function() {
-//     // console.log('h4');
-//   var dateHTML = $('#eventDate').html()
-//   console.log(dateHTML);
-//   var formattedDate = moment(dateHTML).format('dddd, MMM Do');
-//   $('#eventDate').html(formattedDate);
-//   });
-
-//   // var timeHTML = $('#eventTime').html()
-//   // var formattedTime = moment(timeHTML, "hh").format('h:00a');
-//   // $('#eventTime').html(formattedTime)
-
-// });
 
 $(document).on('turbolinks:load', function() {
-  var dateHTML = $('#eventDate').html()
-  var formattedDate = moment(dateHTML).format('dddd, MMM Do');
-  $('#eventDate').html(formattedDate);
+  $('.eventDate').each(function(index, elem) {
+    // console.log('h4');
+    var dateHTML = elem.innerHTML;
+    console.log(dateHTML);
+    if($(elem).hasClass('eventDate')) {
+      var formattedDate = moment(dateHTML).format('dddd, MMM Do');
+      $(elem).html(formattedDate);
+    } 
 
-  var timeHTML = $('#eventTime').html()
-  var formattedTime = moment(timeHTML, "hh").format('h:00a');
-  $('#eventTime').html(formattedTime)
+  });
+  $('.eventTime').each(function(index, elem) {
+    // console.log('h4');
+    var dateHTML = elem.innerHTML;
+    console.log(dateHTML);
+    if($(elem).hasClass('eventTime')) {
+      var formattedTime = moment(dateHTML, "hh").format('h:00a');
+      $(elem).html(formattedTime);
+    } 
+
+  });
 
 });
